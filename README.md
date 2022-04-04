355d598c20c9f529ad5189604064b6244895cc18
LazyLee <36654052+lazylee-l2i@users.noreply.github.com>
2022-04-04 21:25:43 +0900

    google_ver beta
    구글버전 첫 커밋

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # minecraft Mod Translator
- 영어가 딸려서 만든 모드 번역기
+## (부제)영어가 딸려서 만든 모드 번역기
+
+1. `모드`가 1개면 그냥 하겠는데 **pam's harvest**하다가 능지가 딸려 현타와서 만들었습니다.  
+2. 해당 프로그램은 2가지 버전이 존재합니다. `구글 버전`과 `Papago 버전`입니다.  
+3. 현재 `구글 버전`의 경우 `무료번역`은 가능하지만 중간 *timeout*뜨면 에러가 나서 그냥 예외처리로 영어가 그대로 들어가게 해놓았습니다.  
+4. 대신 `Papago 버전`의 경우 머기업답게 준수한 퀄리티의 번역과 깔끔한 API의 도움으로 번역이 아주 잘되나 아쉽게도 돈이 들어가는 관계로 `N Cloud`에서 직접 API 키를 발급받아 사용해주시기 바랍니다. [Papago N Cloud 링크](https://www.ncloud.com/product/aiService/papagoTranslation)
+5. 버전은 **branch**로 나눠놓았습니다.
+  
+# Requirements
+1. commentjson==0.9.0  
+pip install commentjson
+2. googletrans==4.0.0rc1  
+pip install googletrans==4.0.0rc1
+# 사용방법
+1. **mod**폴더에 번역하고자 하는 jar 모드 파일을 넣습니다.
+2. (**cmd** or **powershell**) python main.py
+3. 열심히 기다립니다.
+4. **translated_mod**에 번역된 jar 모드 파일이 있을겁니다.
+5. 이제 그 모드파일을 가져다 적용하고 즐기세요!  
+![0](./mdimg/01.png)
+# 잡담
+제가 생각해도 기능구현만 우선시해서 가독성이 겁나 구린데 죄송합니다.  
+아직 parameter도 설정파일로 안만들었고 UI도 없는데 직장인이라 업데이트가 많이 늦어요.  
+혹시라도 이 프로그램을 사용하시고 수정이나 이슈를 알려주시면 최대한 빨리 반영하도록 하겠습니다.  
+영알못들아... 나에게 힘을 줘..!!
---
+++ main.py
@@ -0,0 +1,64 @@
+import zipfile
+import glob
+import json
+import shutil
+import commentjson
+import googletrans
+
+class JsonExporter:
+    def __init__(self) -> None:
+        self.mod_list = glob.glob("./mod/*.jar")
+        self.translated_list = glob.glob("./translated_mod/*.jar")
+        self.translator = googletrans.Translator()
+
+    def oneFile(self, _file):
+        json_file, lang_path = self.postProcessing(_file=_file)
+        result_json = self.translate(_json_data=json_file)
+        self.saveJar(result_json, _file, lang_path)
+
+    def allFile(self):
+        for file in self.mod_list:
+            zip_path = file.replace("mod", "translated_mod")
+            if zip_path in self.translated_list:
+                continue
+            shutil.copy(file, zip_path.replace(".jar", "_korean.jar"))
+            self.oneFile(zip_path)
+
+    def postProcessing(self, _file :str="jar_file"):
+        json_file = "json_file"
+        file_list = zipfile.ZipFile(_file, 'r')
+        for _file in file_list.namelist():
+            if "en_us.json" in _file:
+                print(_file)
+                try:
+                    json_file = json.loads(file_list.read(_file))
+                except:
+                    json_file = commentjson.loads(file_list.read(_file))
+                    
+                file_list.close()
+                return json_file ,_file
+        print("Here is no Language file")
+
+    def translate(self, _json_data :dict="Json File"):
+        result = _json_data.copy()
+        for _key in _json_data.keys():
+            try:
+                result[_key] = self.translator.translate(_json_data[_key], src="en", dest="ko").text
+            except:
+                print("아 걱정마세요. 평범한 에러메세지입니다. 무료버전의 한계니 그냥 넘어가도록하죠.")
+                result[_key] = _json_data[_key]
+        return result
+
+    def saveJar(self, _result_json :dict, _zip_path :str, _lang_path :str):
+        t_jar = zipfile.ZipFile(_zip_path, 'a')
+        t_jar.writestr(_lang_path.replace("en_us", "ko_kr"), json.dumps(_result_json, indent=4, ensure_ascii=False))
+        t_jar.close()
+
+
+def run():
+    je = JsonExporter()
+    je.allFile()
+
+
+if __name__ == "__main__":
+    run()
